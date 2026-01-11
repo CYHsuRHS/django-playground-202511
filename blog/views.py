ad8c12922cd4ef7eaf5e6909dc06f0f18f98e13f
CYHsuRHS <rhs.hsu@gmail.com>
2026-01-11 20:23:54 +0800

    version 2025121303
    1.使用Django的get_object_or_404函式來處理找不到物件而拋出404的問題
    2.自訂 404 錯誤頁面
    3.示範如何關閉Debug模式

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+# from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from blog.models import Article, Author, Tag
 
@@ -11,9 +12,20 @@ def article_list(request):
     )  # 透過render function將articles變數丟到"articles" template裡面，要渲染的畫面是"blog/article_list.html"，並且挾帶articles變數到"articles" template裡
 
 
+# 原始處裡DoesNotExist問題寫法，需使用try-except
+# def article_detail(request, article_id):
+#     try:
+#         article = Article.objects.get(id=article_id)
+#     except Article.DoesNotExist:
+#         raise Http404
+
+#     return render(request, "blog/article_detail.html", {"article": article})
+
+
 # 用path傳進來的article_id去尋找該篇文章存不存在
 def article_detail(request, article_id):
-    article = Article.objects.get(id=article_id)
+    # get_object_or_404可以給一個queryset或是一個model，目前是給Article這個model，並使用article_id去搜尋，找得到就給物件，找不到就給404
+    article = get_object_or_404(Article, id=article_id)
     return render(request, "blog/article_detail.html", {"article": article})
 
 
@@ -23,7 +35,8 @@ def author_list(request):
 
 
 def author_detail(request, author_id):
-    author = Author.objects.get(id=author_id)
+    # author = Author.objects.get(id=author_id)
+    author = get_object_or_404(Author, id=author_id)
     return render(request, "blog/author_detail.html", {"author": author})
 
 
