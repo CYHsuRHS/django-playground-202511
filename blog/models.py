9fd82b9b1fa6460674344c6a0ba2904ef15b2e2e
CYHsuRHS <rhs.hsu@gmail.com>
2026-01-20 18:33:53 +0800

    version 2025122705
    實作範例：為文章加上建立人欄位
    created_by(null=True, blank=True)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -21,6 +22,13 @@ class Tag(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    created_by = models.ForeignKey(
+        # 關聯到它設定的User
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
